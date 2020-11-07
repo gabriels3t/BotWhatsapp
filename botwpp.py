536fb33d1fdfb416487c64c26693151489666f2c
gabriels3t <gabriels3t@hotmail.com>
2020-11-07 00:08:09 -0300

    bot do whatsapp

---
+++ botwpp.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from time import sleep
+
+class Wpp:
+    def __init__(self):
+        self.mensagem = []
+        while True:
+
+            self.mensagem.append(str(input('Adicionar Mensagem ')))
+            sair = str(input('Adicionar mais Mensagens ? [S/N]')).upper()
+          
+            if sair != 'S' and sair != 'N':
+                while True:
+                   sair = str(input('Por favor Apenas [S/N]').upper())
+                   if sair == 'S' or sair == 'N':
+                       break
+            else:
+                if sair == 'N':
+                    break
+        
+        self.grupo = []
+        while True:
+            self.grupo.append(str(input('Adicionar Grupos ')))
+            sair = str(input('Adicionar mais Mensagens ? [S/N]')).upper()
+           
+            if sair != 'S' and sair != 'N':
+                while True:
+                   sair = str(input('Por favor Apenas [S/N]').upper())
+                   if sair == 'S' or sair == 'N':
+                       break
+            else:
+                if sair == 'N':
+                    break
+
+        opcao = webdriver.ChromeOptions()
+        opcao.add_argument('lang=pt-br')
+        self.drive = webdriver.Chrome(executable_path='//usr/lib/chromium-browser/chromedriver')
+        print(self.mensagem)
+        print(self.grupo)
+
+
+    def comentarios(self):
+        self.drive.get('https://web.whatsapp.com/')
+        print('Esperando O Login...')
+        temp = str(input('Aberte qualquer tecla para continuar'))
+        for grupo in self.grupo:
+            grupo = self.drive.find_element_by_xpath(f"//span[@title='{grupo}']")
+            sleep(3)
+            grupo.click()
+            for msg in self.mensagem:
+                chat = self.drive.find_element_by_class_name('_3uMse')
+                sleep(3)
+                chat.click()
+                sleep(0.5)
+                chat.send_keys(msg)
+                sleep(0.5)
+                botaoEnvia = self.drive.find_element_by_xpath('//span[@data-icon="send"]')
+                sleep(3)
+                botaoEnvia.click()
+                sleep(5)
+           
+
+bot = Wpp()
+bot.comentarios()
